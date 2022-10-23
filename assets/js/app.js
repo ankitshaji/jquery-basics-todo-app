df9550466872c19c46aec853b620bd547e770397
Ankit Shaji <ankitshaji@gmail.com>
2022-10-23 18:47:24 +1300

    cross Todos - simple alternative  using effects method

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,19 +1,10 @@
 //*************************************
 //Check Off Specific Todos By Clicking
 //*************************************
-//jQueryWebApiMethod("css style element selector") -> jQueryListObject with all matching elementObjects
+//jQueryWebApiMethod("css element selector") -> jQueryListObject with all matching elementObjects
 //jQueryListObject.asyncMethod(callback) - setter - addClickEventListener on all elementObjects in jQueryListObject
 $("li").click(function (e) {
   //jQueryWebApiMethod(this keyword - specific left of dot elementObject) -> jQueryListObject with 1 specific elementObject
-  //jQueryListObject.syncMethod("cssPropertyStringObject") - getter - get style css properties on 1 specific elementObject
-  if ($(this).css("color") === "rgb(128, 128, 128)") {
-    //jQueryWebApiMethod(this keyword - specific left of dot elementObject) -> jQueryListObject with 1 specific elementObject
-    //jQueryListObject.syncMethod(styleObject-css properties in camelCase) - setter - set inline style css properties on 1 specific elementObject
-    $(this).css({ color: "black", textDecoration: "none" });
-  } else {
-    //jQueryWebApiMethod(this keyword - specific left of dot elementObject) -> jQueryListObject with 1 specific elementObject
-    //jQueryListObject.syncMethod(styleObject-css properties in camelCase) - setter - set inline style css properties on 1 specific elementObject
-    $(this).css({ color: "gray", textDecoration: "line-through" });
-    //$(this).css("text-decoration", "line-through"); // - alternative setter - (cssPropertyStringObject,cssPropertyValueStringObject)
-  }
+  //jQueryListObject.syncMethod("css class") - setter - set/remove class attribute on 1 specific elementObject
+  $(this).toggleClass("completed");
 });
