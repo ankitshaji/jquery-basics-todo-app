ea7b455ca4cb6424d79b0e56f0b9c2a990e38ca8
Ankit Shaji <ankitshaji@gmail.com>
2022-10-23 22:36:23 +1300

    delete li on span click-e.stopPropagation()+effects+traversing method

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,9 +2,28 @@
 //Check Off Specific Todos By Clicking
 //*************************************
 //jQueryWebApiMethod("css element selector") -> jQueryListObject with all matching elementObjects
-//jQueryListObject.asyncMethod(callback) - setter - addClickEventListener on all elementObjects in jQueryListObject
+//jQueryListObject.asyncMethod(callback) - setter - execute addEventListener with clickEventStringObject on all elementObjects in jQueryListObject
 $("li").click(function (e) {
   //jQueryWebApiMethod(this keyword - specific left of dot elementObject) -> jQueryListObject with 1 specific elementObject
   //jQueryListObject.syncMethod("css class") - setter - set/remove class attribute on 1 specific elementObject
   $(this).toggleClass("completed");
+});
+
+//**************************
+//Click on X to delete Todo
+//**************************
+//jQueryWebApiMethod("css element selector") -> jQueryListObject with all matching elementObjects
+//jQueryListObject.asyncMethod(callback) - setter - execute addEventListener with clickEventStringObject on all elementObjects in jQueryListObject
+$("span").click(function (e) {
+  //jQueryWebApiMethod(this keyword - specific left of dot elementObject) -> jQueryListObject with 1 specific elementObject
+  //jQueryListObject.syncMethod() -> jQueryListObject with 1 specific parentElementObject
+  //parentjQueryListObject.asyncMethod(milliseconds,callback) - execute callback after set millisecond
+  $(this)
+    .parent()
+    .fadeOut(500, function () {
+      //jQueryListObject.syncMethod() - removes the 1 specific parentElementObject
+      $(this).remove();
+    });
+  //clickEventObject.syncMethod()
+  e.stopPropagation(); //prevent the parent elements that executed addEventListner with clickEventStringObject from running its callback - ie prevent event bubbling up
 });
